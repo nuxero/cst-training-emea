029c9d3c38d8652af23055d3dd322a005073e083
Roy Bns <roy.benshushan@vonage.com>
2020-01-27 15:36:13 +0200

    Updating server

--- video/application_server/routes/event.js
+++ video/application_server/routes/event.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-//4. Parse incoming events - configure your Session Monitoring url in TKBX dashboard... Inspect the events
+//4. Parse incoming events - configure your Session Monitoring url in TKBX dashboard... Inspect the events!
 router.post('/', function(req, res, next) {
   console.log(JSON.stringify(req.body));
 });
